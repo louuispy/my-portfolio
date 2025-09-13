7e6546533bac0183e5100ab174e9028414a53170
Luis <luishenriquesousay.t.2018@gmail.com>
2025-09-13 18:32:17 -0300

    create navbar, modify scrollbar and selection color and add navigation to navbar

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const fadeSections = document.querySelectorAll(
-    '.container__about__me, .container__technologies, .container__experience, .container__projects'
+    ".container__about__me, .container__technologies, .container__experience, .container__projects, .footer__contact"
   );
 
   const observer = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('is-visible');
+          entry.target.classList.add("is-visible");
           observer.unobserve(entry.target);
         }
       });
     },
     {
       threshold: 0.1,
-      rootMargin: '0px 0px -50px 0px' 
+      rootMargin: "0px 0px -50px 0px",
     }
   );
 
   fadeSections.forEach((section) => observer.observe(section));
+});
+
+document.querySelectorAll(".nav__links a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const navbarHeight = document.querySelector(".nav__bar").offsetHeight;
+
+    const offsetPosition =
+      targetElement.getBoundingClientRect().top +
+      window.pageYOffset -
+      navbarHeight;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
 });
